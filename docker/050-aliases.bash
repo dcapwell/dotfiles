06baa438139d027e839746a21ec528bfbf37b2e4
David Capwell <dcapwell@gmail.com>
2015-01-05 09:37:42 -0800

    added boot2docker restart

--- docker/050-aliases.bash
+++ docker/050-aliases.bash
@@ -1,3 +1,8 @@
 # run the current directory in a docker container
 # maven and gradle homes are also mounted
 alias docker-env='docker run -t -i -w $PWD -v $PWD:$PWD -v $HOME/.m2:/root/.m2 -v $HOME/.gradle:/root/.gradle'
+
+if type boot2docker >/dev/null 2>&1 ; then
+  # restart boot2docker
+  alias boot2docker-restart="boot2docker down && boot2docker up"
+fi
