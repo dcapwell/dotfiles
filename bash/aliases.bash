19f7f841a113709f769f3a1d44906fbc7976efce
dcapwell <dcapwell@gmail.com>
2015-01-04 23:06:03 -0800

    following dotfile convention, slowly forking Bash-Profile

---
+++ bash/aliases.bash
@@ -0,0 +1,10 @@
+# Easier navigation: .., ..., ...., ....., ~ and -
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ~="cd ~" # `cd` is probably faster to type though
+alias -- -="cd -"
+
+# Reload the shell
+alias reload="exec $SHELL -l"
