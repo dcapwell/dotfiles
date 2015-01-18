2c3cccec4e4a69b2a943dfd5296bc8ba8419ab97
David Capwell <dcapwell@gmail.com>
2015-01-17 16:42:18 -0800

    remove m2 and gradle from docker-env by default. added --rm

--- docker/050-aliases.bash
+++ docker/050-aliases.bash
@@ -1,6 +1,6 @@
 # run the current directory in a docker container
 # maven and gradle homes are also mounted
-alias docker-env='docker run -t -i -w $PWD -v $PWD:$PWD -v $HOME/.m2:/root/.m2 -v $HOME/.gradle:/root/.gradle'
+alias docker-env='docker run -t -i --rm -w $PWD -v $PWD:$PWD'
 
 if type boot2docker >/dev/null 2>&1 ; then
   # restart boot2docker
