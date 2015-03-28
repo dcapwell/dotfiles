f06bb20fe5007c1f691a3506346d2c9aadcc3b7d
David Capwell <dcapwell@gmail.com>
2015-03-27 18:17:22 -0700

    get the abspath for intellij

--- intellij/080-functions.bash
+++ intellij/080-functions.bash
@@ -1,7 +1,8 @@
 # Tries to open the given path in Intellij (idea)
 # If no arguments given, assumes $PWD
 intellij() {
-  local readonly path="${1:$PWD}"
+  local path="${1:-$PWD}"
+  path=$(abspath "$path")
   if is_osx ; then
     open -a "$(ls -rd /Applications/IntelliJ\ IDEA\ * | head -n1)" "$path"
   else
