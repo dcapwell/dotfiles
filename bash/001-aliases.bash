12601108fb56c5a6061b4d4fcb47cf15d68ee2a4
David Capwell <dcapwell@gmail.com>
2015-07-15 09:10:14 -0700

    created simple hex2dec util alias

--- bash/001-aliases.bash
+++ bash/001-aliases.bash
@@ -24,3 +24,5 @@ alias time=/usr/bin/time
 # For example, to list all directories that contain a certain file:
 # find . -name .gitattributes | map dirname
 alias map="xargs -n1"
+
+alias hex2dec='printf "%d\n"'
