1e0f704092dd58aa1d072cc9a4d0edfa4bd199a4
David Capwell <dcapwell@gmail.com>
2016-01-18 11:51:38 -0800

    added docker-machine support

--- docker/050-boot2docker-export.bash
+++ docker/050-boot2docker-export.bash
@@ -1,3 +1,5 @@
-if type boot2docker 1>/dev/null 2>/dev/null ; then
+if type docker-machine 1>/dev/null 2>/dev/null ; then
+  eval $(docker-machine env docker 2>/dev/null)
+elif type boot2docker 1>/dev/null 2>/dev/null ; then
   eval $(boot2docker shellinit 2>/dev/null)
 fi
