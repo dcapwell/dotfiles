8d8e7ba24b834a2e3d1f310c8c16aa64a496fa91
David Capwell <dcapwell@gmail.com>
2016-04-04 14:13:15 -0700

    added function to cleanup dangling docker images

--- docker/050-functions.bash
+++ docker/050-functions.bash
@@ -17,6 +17,10 @@ function docker_clean_images() {
   docker images | grep -e '^<none>' | awk '{print $3}' | xargs docker rmi
 }
 
+function docker_clean_images_dangling() {
+  docker rmi $(docker images -f dangling=true -q)
+}
+
 docker_api() {
   boot2docker ssh curl --silent --show-error --globoff --insecure --cert $DOCKER_CERT_PATH/cert.pem --key $DOCKER_CERT_PATH/key.pem "$(echo $DOCKER_HOST | sed 's;tcp;https;')/$@"
 }
