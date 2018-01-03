453d4ba7808c1b5b4c67967e52c2a86ddd36549f
David Capwell <dcapwell@gmail.com>
2018-01-02 16:03:00 -0800

    if docker socket is found use that directly

--- docker/050-boot2docker-export.bash
+++ docker/050-boot2docker-export.bash
@@ -1,4 +1,8 @@
-if type docker-machine 1>/dev/null 2>/dev/null ; then
+if [ -e /var/run/docker.sock ] ; then
+  unset DOCKER_CERT_PATH
+  unset DOCKER_TLS_VERIFY
+  export DOCKER_HOST=unix:///var/run/docker.sock
+elif type docker-machine 1>/dev/null 2>/dev/null ; then
   eval $(docker-machine env docker 2>/dev/null)
 elif type boot2docker 1>/dev/null 2>/dev/null ; then
   eval $(boot2docker shellinit 2>/dev/null)
