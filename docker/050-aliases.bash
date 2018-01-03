9596e4dc8981435156ad037ebf0eec2e04f4bf6d
David Capwell <dcapwell@gmail.com>
2018-01-02 16:02:32 -0800

    added docker alias to visualize images

--- docker/050-aliases.bash
+++ docker/050-aliases.bash
@@ -2,6 +2,9 @@
 # maven and gradle homes are also mounted
 alias docker-env='docker run -t -i --rm -w $PWD -v $PWD:$PWD'
 
+alias dockerviz='docker run --rm -v /var/run/docker.sock:/var/run/docker.sock nate/dockviz images -t'
+alias dockviz='docker run --rm -v /var/run/docker.sock:/var/run/docker.sock nate/dockviz images -t'
+
 if type boot2docker >/dev/null 2>&1 ; then
   # restart boot2docker
   alias boot2docker-restart="boot2docker down && boot2docker up"
