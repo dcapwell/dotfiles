c33442c0f693bd481c28587d4e30c48fe505fe5d
David Capwell <dcapwell@gmail.com>
2020-02-06 10:42:25 -0800

    added ramdisk mounting to osx

--- osx/050-functions.bash
+++ osx/050-functions.bash
@@ -4,4 +4,11 @@ if is_osx ; then
   kill_display() {
     killall WindowServer
   }
+
+  create_ramdisk() {
+    # mount with 30g
+    hdiutil attach -nomount ram://62914560
+    diskutil erasevolume HFS+ "ramdisk" /dev/disk2
+    echo "created volume at /Volumes/ramdisk/"
+  }
 fi
