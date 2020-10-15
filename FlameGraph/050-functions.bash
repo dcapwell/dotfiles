1142a47653e2a3fd64eb67ed08084a45e457c56b
David Capwell <dcapwell@gmail.com>
2020-10-15 09:58:52 -0700

    added new java flamegraph commands

--- FlameGraph/050-functions.bash
+++ FlameGraph/050-functions.bash
@@ -1,1 +1,14 @@
-export PATH="$PATH:/opt/FlameGraph/FlameGraph_master"
+javaflame() {
+  local -r path="$1"
+
+  flamegraph.pl --color java "$path" > "$path.svg"
+}
+
+javaflame_diff() {
+  local -r name="$1"
+  local -r first="$2"
+  local -r second="$3"
+
+  difffolded.pl "$first" "$second" | flamegraph.pl > "${name}.svg"
+  difffolded.pl -n "$second" "$first" | flamegraph.pl --negate > "${name}-negate.svg"
+}
