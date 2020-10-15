6908af78fc3b3f2db9e9223bd207c891a16f43f1
David Capwell <dcapwell@gmail.com>
2020-10-15 10:27:30 -0700

    javadefaults shows more hidden flags.  added aliases for eclipse memory analyzer

--- jdk/050-aliases.bash
+++ jdk/050-aliases.bash
@@ -1,1 +1,6 @@
-alias javadefault='java -XX:+UnlockDiagnosticVMOptions -XX:+PrintFlagsFinal 2>/dev/null'
+alias javadefault='java -XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+PrintFlagsFinal 2>/dev/null'
+if [ -e $HOME/Applications/mat.app/Contents/Eclipse/plugins/org.eclipse.equinox.launcher_*.jar ]; then
+  alias eclipse_memory_analyzer='java -XstartOnFirstThread -Xmx${HEAP_ANALYZER_XMX:-20g} -jar $HOME/Applications/mat.app/Contents/Eclipse/plugins/org.eclipse.equinox.launcher_*.jar'
+  alias mat=eclipse_memory_analyzer
+  alias java_memory_analyzer=eclipse_memory_analyzer
+fi
