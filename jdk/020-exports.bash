fca2d29d44bb916af01fa094b0b084bd9269437e
David Capwell <dcapwell@gmail.com>
2023-05-18 14:20:39 -0700

    add -Dchronicle.analytics.disable=true to disable chronicle tracking...

--- jdk/020-exports.bash
+++ jdk/020-exports.bash
@@ -1,3 +1,3 @@
 # Tells java to prefer ipv4
 # http://superuser.com/questions/453298/how-to-force-java-to-use-ipv4-instead-ipv6
-export _JAVA_OPTIONS="-Djava.net.preferIPv4Stack=true"
+export _JAVA_OPTIONS="-Djava.net.preferIPv4Stack=true -Dchronicle.analytics.disable=true"
